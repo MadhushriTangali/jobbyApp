e35601166414ca4c4eec348c2b94639310f95fbd
Madhushri Tangali <madhushri.kle@gmail.com>
2025-07-11 20:30:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Interview Insights Flow")
+st.markdown("This is a simple Streamlit app deployed from GitHub.")
+
+# Add your logic or call functions from other scripts
